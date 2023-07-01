d9e0791af8852ded59ff97cb47fb08eef435296a
rungene <rungenesamuel@gmail.com>
2023-07-01 16:57:08 +0000

    Adding name field

--- file-upload/models.py
+++ file-upload/models.py
@@ -1,11 +1,12 @@
 from app import db
 
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(120), index=True)
+    password = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
