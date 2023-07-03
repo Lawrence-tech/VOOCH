db2667f864a62ab9750b23bbe7fbf91fad26ce7a
rungene <rungenesamuel@gmail.com>
2023-07-03 01:22:25 +0000

    Corretions-Typo

--- file-upload/models.py
+++ file-upload/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     name = db.Column(db.String(120), index=True)
     password = db.Column(db.String(128))
-    artorks = db.relationship('Artwork', backref='uploader', lazy='dynamic')
+    artworks = db.relationship('Artwork', backref='uploader', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
