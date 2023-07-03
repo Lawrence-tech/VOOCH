2e7b539dc339f8bb7398709f99275ed159382301
Lawrence-tech <lawrencesagini2@gmail.com>
2023-07-03 20:16:04 +0300

    check back to the previous Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,43 +2,55 @@ import React, { useState } from 'react';
 import { logo, lock, hamburgerMenu, close } from '../assets';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => setToggle(!toggle);
 
-  const [toggle,setToggle]=useState(false)
-  const handleClick = ()=> setToggle(!toggle)
+  return (
+    <div className="w-full h-16 bg-white border-b">
+      <div className="max-w-7xl mx-auto px-4 flex justify-between items-center h-full">
+        <img src={logo} className="h-6" />
 
-return (
-  <><nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-      <a href="https://flowbite.com" class="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-      </a>
-      <div class="flex items-center">
-        <a href="tel:5541251234" class="mr-6 text-sm  text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-        <a href="#" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
-      </div>
-    </div>
-  </nav><nav class="bg-gray-50 dark:bg-gray-700">
-      <div class="max-w-screen-xl px-4 py-3 mx-auto">
-        <div class="flex items-center">
-          <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
-            <li>
-              <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
-            </li>
-            <li>
-              <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
-            </li>
-            <li>
-              <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
-            </li>
+        <div className="hidden md:flex items-center space-x-4">
+          <ul className="flex gap-4">
+            <li>Home</li>
+            <li>About</li>
+            <li>Support</li>
+            <li>Platform</li>
+            <li>Pricing</li>
           </ul>
         </div>
+
+        <div className="hidden md:flex">
+          <button className="flex items-center bg-transparent px-6 gap-2">
+            <img src={lock} alt="Lock Icon" />
+            Login
+          </button>
+          <button className="px-8 py-3 rounded-md bg-green-500 text-white font-bold">Sign Up For Free</button>
+        </div>
+
+        <div className="md:hidden" onClick={handleClick}>
+          <img src={toggle ? close : hamburgerMenu} alt="Menu Icon" />
+        </div>
       </div>
-    </nav></>
-)
-}
+
+      <div className={toggle ? 'p-4 bg-white w-full border-b block' : 'hidden'}>
+        <ul>
+          <li className="p-4 hover:bg-gray-100">Home</li>
+          <li className="p-4 hover:bg-gray-100">About</li>
+          <li className="p-4 hover:bg-gray-100">Support</li>
+          <li className="p-4 hover:bg-gray-100">Platform</li>
+          <li className="p-4 hover:bg-gray-100">Pricing</li>
+          <div className="flex flex-col my-4 gap-4">
+            <button className="border border-green-500 flex justify-center items-center bg-transparent px-6 gap-2 py-4">
+              <img src={lock} alt="Lock Icon" />
+              Login
+            </button>
+            <button className="px-8 py-5 rounded-md bg-green-500 text-white font-bold">Sign Up For Free</button>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
