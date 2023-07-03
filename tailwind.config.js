80f825ce491fc1af0e3a201129398c78d730e313
Lawrence-tech <lawrencesagini2@gmail.com>
2023-07-03 19:53:49 +0300

    add flowbite and modiefied related files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('flowbite/plugin')
     // Other plugins...
   ],
 }
