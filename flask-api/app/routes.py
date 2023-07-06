181d6ddb52ff982e3edff8e738b6c3324fcf1e20
rungene <rungenesamuel@gmail.com>
2023-07-06 13:12:57 +0000

    Corrections

--- flask-api/app/routes.py
+++ flask-api/app/routes.py
@@ -17,7 +17,7 @@ app.config["ALLOWED_FILE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF", "jpg"]
 
 
 @app.route("/", strict_slashes=False)
-@app.route('/index' strict_slashes=False)
+@app.route('/index', strict_slashes=False)
 def index():
     """Index route"""
     return render_template('upload.html')
