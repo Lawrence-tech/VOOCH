f27e7699a9475ebc0cdc428b649dcb0c4dc1f93e
Lawrence-tech <lawrencesagini2@gmail.com>
2023-07-15 00:25:36 +0300

    connecting backend to frontend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Add the CORS middleware after creating the Flask app instance
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
