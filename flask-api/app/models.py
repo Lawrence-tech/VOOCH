5bac0762de1236e6811dbdb7e7d8dc335ba3806a
rungene <rungenesamuel@gmail.com>
2023-07-16 15:03:35 +0000

    adding foreign key on artworks model to join reviwer model

--- flask-api/app/models.py
+++ flask-api/app/models.py
@@ -53,7 +53,7 @@ class Reviewer(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     name = db.Column(db.String(120), index=True)
     password_hash = db.Column(db.String(128))
-    artworks = db.relationship('Artwork', backref='uploader', lazy='dynamic')
+    artworks = db.relationship('Artwork', backref='reviwer', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -88,6 +88,7 @@ class Artwork(db.Model):
     image = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    reviwer_id = db.Column(db.Integer, db.ForeignKey('reviwer.id'))
 
     def __repr__(self):
         return f'<Artwork {self.title}>'
