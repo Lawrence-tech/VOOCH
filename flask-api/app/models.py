598a78c61cb1877002affb2dd1c76cf38a7e5e5c
rungene <rungenesamuel@gmail.com>
2023-07-16 16:08:44 +0000

    Correction typo

--- flask-api/app/models.py
+++ flask-api/app/models.py
@@ -53,7 +53,7 @@ class Reviewer(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     name = db.Column(db.String(120), index=True)
     password_hash = db.Column(db.String(128))
-    artworks = db.relationship('Artwork', backref='reviwer', lazy='dynamic')
+    artworks = db.relationship('Artwork', backref='reviewer', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
